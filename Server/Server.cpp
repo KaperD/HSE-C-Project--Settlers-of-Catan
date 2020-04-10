013c7b4abf7f175039b4ef65b6d03f59fe147f03
Danil <dkbubnov@edu.hse.ru>
2020-04-10 11:05:28 +0500

    Changes for server

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <exception>
+#include <chrono>
+#include <thread>
+#include <mutex>
 
 #include <grpc/grpc.h>
 #include <grpcpp/server.h>
@@ -8,6 +11,7 @@
 #include <grpcpp/security/server_credentials.h>
 #include "game.grpc.pb.h"
 #include "EventQueue.h"
+#include "spinlock.h"
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -24,14 +28,38 @@ using game::Build;
 using game::Player;
 using game::Network;
 
+
 class GameServerImpl final : public Network::Service {
 public:
     GameServerImpl() : events_(4), currentPlayerNumber_(0) { }
+
+    void RefreshGame() {
+        std::lock_guard<threads_sync::spinlock> guard(spin_);
+        if (isRun_ == true) {
+            Event endgame;
+            endgame.set_type(EventType::ENDGAME);
+            for (int k = 0; k < 3; ++k) {
+                endgame.set_playerid(k + 1);
+                events_[k].clear();
+                events_[k].push(endgame);
+            }
+            isRun_ = false;
+        }
+    }
 private:
     Status Register(::grpc::ServerContext* context, const Void* request, OrderInfo* response) override {
+        std::lock_guard<threads_sync::spinlock> guard(spin_);
+        if (isRun_ == false) {
+            for (int k = 0; k < 3; ++k) {
+                events_[k].clear();
+            }
+            currentPlayerNumber_ = 0;
+        }
+        isRun_ = true;
         int id = currentPlayerNumber_++;
         if (id > 2) {
             id %= 3;
+            currentPlayerNumber_ %= 3;
             events_[id].clear();
         }
         id %= 3;
@@ -63,23 +91,37 @@ private:
 
         return Status::OK;
     }
-  
+
 private:
     std::vector<EventQueue> events_;
-    std::atomic_int currentPlayerNumber_;
+    int currentPlayerNumber_;
+    bool isRun_ = false;
+    threads_sync::spinlock spin_;
 };
+
+
+void refresher(GameServerImpl* server) {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(30));
+        server->RefreshGame();
+    }
+}
 
  
 void RunServer() {
-  std::string server_address("68.183.30.230:50051");
-  GameServerImpl service;
+    std::string server_address("68.183.30.230:50051");
+    GameServerImpl service;
 
-  ServerBuilder builder;
+    ServerBuilder builder;
 
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  server->Wait();
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+
+    std::thread ref(refresher, &service);
+    ref.detach();
+
+    server->Wait();
 }
 
 int main() {
